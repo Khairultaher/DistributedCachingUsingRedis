a4ed7590648be08037ff2f0501bc0a73564dc8ee
Khairul Taher <khairultaher@outlook.com>
2021-06-09 17:04:19 +0600

    2021-06-08.4

--- src/RedisSample/Program.cs
+++ src/RedisSample/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +28,7 @@ namespace RedisSample
         public void ReadData()
         {
             var cache = RedisConnectorHelper.Connection.GetDatabase();
-            var devicesCount = 10;
+            var devicesCount = 1;
             for (int i = 1; i <= devicesCount; i++)
             {
                 var value = cache.StringGet($"key{i}");
@@ -32,15 +36,16 @@ namespace RedisSample
             }
         }
 
-        public void SaveBigData()
+        public  void SaveBigData()
         {
-            var devicesCount = 10;
+            var devicesCount = 1;
             var rnd = new Random();
             var cache = RedisConnectorHelper.Connection.GetDatabase();
 
             for (int i = 1; i <= devicesCount; i++)
             {
-                var value = rnd.Next(0, 10000);
+                var value = File.ReadAllText("E:\\PRACTICE\\PERFORMANCE\\DistributedCachingUsingRedis\\src\\RedisSample\\data.txt");
+                //var value = rnd.Next(0, 10000);
                 //var value = i;
                 cache.StringSet($"key{i}", value);
             }
